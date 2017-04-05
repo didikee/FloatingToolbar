0bdd1b5a747371b6ea1381249a5011849487473f
didikee <didikee@qq.com>
2017-04-05 13:24:32 +0800

    已经能展示toolbar TODO： 动画，显示位置

--- app/src/main/java/com/didikee/floatingtoolbar/MainActivity.java
+++ app/src/main/java/com/didikee/floatingtoolbar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.didikee.floatingtoolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
@@ -13,12 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        startActivity(new Intent(this,TestActivity.class));
-//
-//        finish();
-        mWebView = ((WebView) findViewById(R.id.webView));
+        startActivity(new Intent(this,TestActivity.class));
 
-        mWebView.loadUrl("file:///android_asset/Android Wikipedia.html");
+        finish();
+//        mWebView = ((WebView) findViewById(R.id.webView));
+//
+//        mWebView.loadUrl("file:///android_asset/Android Wikipedia.html");
 
     }
 }
--- app/src/main/java/com/didikee/floatingtoolbar/TestActivity.java
+++ app/src/main/java/com/didikee/floatingtoolbar/TestActivity.java
@@ -2,6 +2,8 @@ package com.didikee.floatingtoolbar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -10,27 +12,35 @@ import com.didikee.library.FloatingToolbar;
 public class TestActivity extends AppCompatActivity {
 
     private FrameLayout container;
+    private FloatingToolbar floatingToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 //        container = (FrameLayout) findViewById(R.id.container);
-
+        floatingToolbar = new FloatingToolbar(this,getWindow());
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                floattoolbar();
             }
         });
 
     }
 
     private void floattoolbar(){
-        FloatingToolbar floatingToolbar =new FloatingToolbar(this,getWindow());
+
+        floatingToolbar.show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.test,menu);
+        floatingToolbar.setMenu(menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     private void setLollipopAnimator() {
     }
